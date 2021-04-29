1aa7a3f3f1ed47844529a257c28ff5ca8f9422b1
AlexVipond <alex.r.vipond@gmail.com>
2021-04-28 23:11:09 -0600

    reconfigure tailwind, scaffold app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  mode: 'jit'
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.blueGray,
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }

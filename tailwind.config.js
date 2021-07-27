8cf1fbb35899490b348514c8de9b7ca6be5aa838
AlexVipond <alex.r.vipond@gmail.com>
2021-07-27 10:46:12 -0500

    jit support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+  mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -9,9 +10,6 @@ module.exports = {
         gray: colors.blueGray,
       }
     }
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }

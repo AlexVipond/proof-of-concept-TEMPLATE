e5d440630f6fb4df5bcee9302a1a3940df33b502
AlexVipond <alex.r.vipond@gmail.com>
2021-10-08 00:00:01 -0400

    upgrade deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  mode: 'jit',
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        gray: colors.blueGray,
+        gray: colors.slate,
       }
     }
   },

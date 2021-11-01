518309be2b49e4c2017f1c37e275942f10961e6f
AlexVipond <alex.r.vipond@gmail.com>
2021-11-01 17:32:18 -0400

    script setup

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,6 @@
   </main>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
 
-export default defineComponent({
-  setup () {
-    
-  }
-})
 </script>
